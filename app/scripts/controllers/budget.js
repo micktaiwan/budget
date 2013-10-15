74cb7fe06fae8896ee77864f821c55aae33bb265
Mickael Faivre-Macon <faivrem@gmail.com>
2013-10-15 06:37:38 +0200

    the form works

--- app/scripts/controllers/budget.js
+++ app/scripts/controllers/budget.js
@@ -2,6 +2,9 @@
 
 TODO:
 - dynamic data
+- form validation
+- color the amounts in the timeline
+- correct datepicker
 - initialize current balance by asking the user how much he have now on his account
 - timeline
 - one time in/outcome with date ( to be able to remember checks in the future)
@@ -16,5 +19,9 @@ angular.module('budgetApp')
 
     $scope.incomeTotal  = $scope.lines.filter(function(a){return a.type=='I' || a.type=='i'}).reduce(function(a,b) {return a+b.amount;}, 0);
     $scope.outcomeTotal = $scope.lines.filter(function(a){return a.type=='O' || a.type=='o'}).reduce(function(a,b) {return a+b.amount;}, 0);
+
+    $scope.addItem = function() {
+    	$scope.lines.push({label: $scope.label, amount: $scope.amount, type: $scope.type, date: $scope.date});
+    	};
 
   });
